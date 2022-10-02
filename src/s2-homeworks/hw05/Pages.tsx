cea55d8cc42b883e671538b8052ff039055bfc03
Andrey Malin <andreu6454@gmail.com>
2022-10-03 01:34:04 +0300

    HW5 fixed

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -4,6 +4,7 @@ import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
+import {v1} from "uuid";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -19,8 +20,8 @@ function Pages() {
                 <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-                <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>} key={PATH.JUNIOR}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>} key={v1()}/>
 
                 <Route path={'/*'} element={<Error404/>}/>
             </Routes>
