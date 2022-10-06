d207beea3e916cd24c0a7bd92e340a12d8391c7e
Andrey Malin <andreu6454@gmail.com>
2022-10-06 22:06:35 +0300

    HW10 Done

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <div className={s.loader}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+)
